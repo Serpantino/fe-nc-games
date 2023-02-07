1cacac7aaed5b9f1fbeaf6f12e2bff1a8b0475fd
Serpantino <serpantino@msn.com>
2023-02-07 17:18:38 +0000

    MVP working

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,7 +1,81 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import styles from "./Review.module.css";
 
+export default function Review({ selectedReview }) {
+  const {
+    review_id,
+    category,
+    comment_count,
+    created_at,
+    designer,
+    owner,
+    review_body,
+    review_img_url,
+    title,
+    votes,
+  } = selectedReview;
 
-export default function Review() {
-    return (
-        <h2>Individual Review Page</h2>
-    )
+  let params = useParams();
+  const reviewPostDate = created_at
+    .split("")
+    .slice(0, 10)
+    .join("")
+    .split("-")
+    .reverse()
+    .join("-");
+
+  return (
+    <article className={styles.page_review}>
+      <header className={styles.review_header}>
+        <h2>{title}</h2>
+        <cite>By: {designer}</cite> {/*TODO clickable designer to filter*/}
+        <div className={styles["container_header-inner"]}>
+          <img
+            src={review_img_url}
+            aria-hidden="true"
+            alt="Photo (supposedly) of the game"
+            className={styles["image_single-review"]}
+          />
+          <h4>
+            Review By: <cite>{owner}</cite> <br />
+            <small>Created at: {reviewPostDate}</small>
+          </h4>{" "}
+          {/*TODO Add link to user*/}
+        </div>
+      </header>
+      <section className={styles["container_review-body"]}>
+        <p>{review_body}</p>
+      </section>
+      <section className={styles["container_review-comments"]}>
+        <div>
+          <h4>Comment Placeholder</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+            incidunt? Distinctio unde harum cum dicta officia dignissimos
+            necessitatibus, vel natus, deleniti autem repellendus laborum nobis!
+            Sunt modi explicabo asperiores laudantium.
+          </p>
+        </div>
+        <div>
+          <h4>Comment Placeholder</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+            incidunt? Distinctio unde harum cum dicta officia dignissimos
+            necessitatibus, vel natus, deleniti autem repellendus laborum nobis!
+            Sunt modi explicabo asperiores laudantium.
+          </p>
+        </div>
+        <div>
+          <h4>Comment Placeholder</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+            incidunt? Distinctio unde harum cum dicta officia dignissimos
+            necessitatibus, vel natus, deleniti autem repellendus laborum nobis!
+            Sunt modi explicabo asperiores laudantium.
+          </p>
+        </div>
+      </section>
+    </article>
+  );
 }
--- src/pages/ReviewList.jsx
+++ src/pages/ReviewList.jsx
@@ -1,20 +1,21 @@
 import { FetchReviews } from "../components/FetchData";
 import { useState, useEffect, useRef } from "react";
-import styles from './ReviewList.module.css';
+import { Link, redirect, useNavigate } from "react-router-dom";
+import styles from "./ReviewList.module.css";
 
-export default function ReviewList() {
+export default function ReviewList({setIndividualPage, setSelectedReview, selectedReview}) {
   const [reviews, setReviews] = useState([]);
   let notInitialRender = useRef(false);
-
+  const navigatePage = useNavigate();
+  
   const [reviewsJSX, setReviewsJSX] = useState([]);
-
+  
   useEffect(() => {
     if (notInitialRender.current) {
       buildReviewsJSX(reviews);
-  
     } else {
       notInitialRender.current = true;
-
+      
       console.log("1st render");
       FetchReviews().then((reviewData) => {
         console.log(reviewData);
@@ -22,21 +23,55 @@ export default function ReviewList() {
       });
     }
   }, [reviews]);
-
+  
   function buildReviewsJSX() {
     const tempReviewArray = reviews.map((review) => {
-      
       return (
         <li key={review.review_id} className={styles.list_review}>
-          <img src={review.review_img_url} aria-hidden="true" alt={`Photo of ${review.title}`} className={styles['image_reviews-list']}/>
-          <p aria-label="A link to the review of: " className={styles.linkto_review}>
-            {review.title}
-          </p>
+          <img
+            src={review.review_img_url}
+            aria-hidden="true"
+            alt={`Photo of ${review.title}`}
+            className={styles["image_reviews-list"]}
+            />
+          <Link
+            to={`/review/${review.review_id}`}
+            className={styles.linkto_review}
+            onClick={(e) => handleSelectedReview(e, review)}
+            >
+            <p
+              aria-label="A link to the review of: "
+              className={styles.linkto_review}
+              >
+              {review.title}
+            </p>
+          </Link>
         </li>
       );
     });
-    return setReviewsJSX(tempReviewArray);
+    setReviewsJSX(tempReviewArray);
+  }
+  
+  function handleSelectedReview(e, review) {
+    console.log('review in handleSelect', review);
+    // e.preventDefault();
+    setIndividualPage(true);
+    setSelectedReview(review);   
+    // <article
+    //   className={styles[("card_individual-review", "hidden")]}
+    //   aria-label={`review of ${review.title}`}
+    // ></article>
+    
+  }
+  
+  
+  if(reviews.length > 0) {
+    
   }
 
-  return <ul className={styles['ul-container_reviews']}>{reviewsJSX.length > 0 ? reviewsJSX : "....Loading"}</ul>;
+  return (
+    <ul className={styles["ul-container_reviews"]}>
+      {reviewsJSX.length > 0 ? reviewsJSX : "....Loading"}
+    </ul>
+  );
 }
